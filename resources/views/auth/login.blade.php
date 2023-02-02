25e4ee17197750c00aa55a3e23d28474813c0cf1
akmalranyan <akmalranyan@gmail.com>
2023-02-02 14:38:45 +0700

    made login views

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.app')
+@section('main')
+<div class="mt-5 mx-auto" style="width: 380px">
+    @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
+    <div class="card">
+        <div class="card-body">
+            <form action="{{ route('login') }}" method="POST">
+                @csrf
+                <div class="mb-3">
+                    <label for="" class="form-label">Name</label>
+                    <input name="name" type="text" class="form-control">
+                    @error('name')
+                    <span class="text-danger">
+                        {{ $message }}
+                    </span>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                    <label for="" class="form-label">Email</label>
+                    <input name="email" type="email" class="form-control">
+                    @error('email')
+                    <span class="text-danger">
+                        {{ $message }}
+                    </span>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                    <label for="" class="form-label">Password</label>
+                    <input name="password" type="password" class="form-control">
+                    @error('password')
+                    <span class="text-danger">
+                        {{ $message }}
+                    </span>
+                    @enderror
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    </div>
+</div>
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 
 <div class="border rounded mt-5 mx-auto d-flex flex-column align-items-stretch bg-white" style="width: 380px;">
         <div class="d-flex justify-content-between flex-shrink-0 p-3 link-dark  border-bottom">
-            <span class="fs-5 fw-semibold">Task Management</span>
-        </div>
+            <span class="fs-5 fw-semibold">Welcome to Task Management, {{ Auth::user()->name }} </span>
+        </div> 
     </div>
     @endsection
